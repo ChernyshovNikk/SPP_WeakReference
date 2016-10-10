dba006a874143a250fa2a11bf88d5c7ea06c2be8
Nikita Chernyshov <NikkOrange96@gmail.com>
2016-10-10 11:16:56 +0300

    Start_Part2

--- WeakReference/WeakReference/WeakDelegate.cs
+++ WeakReference/WeakReference/WeakDelegate.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WeakReference
+namespace WeakReferences
 {
     class WeakDelegate
     {
+        WeakReference weakReference;
+        MethodInfo method;
+        CreateDelegate newDelegate;
+       
+        public WeakDelegate(Delegate currentDelegate)
+        {
+            weakReference = new WeakReference(currentDelegate.Target);
+            method = currentDelegate.Method;
+            newDelegate = new CreateDelegate();
+        }
+
+        private class CreateDelegate
+        {
+
+        }       
     }
 }
