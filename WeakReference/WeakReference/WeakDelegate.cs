368cef4a1bacb8ba0468c6c6f11bd521355c2c92
Nikita Chernyshov <NikkOrange96@gmail.com>
2016-10-13 08:52:34 +0300

    MakeWeak
    MakeWeak

--- WeakReference/WeakReference/WeakDelegate.cs
+++ WeakReference/WeakReference/WeakDelegate.cs
@@ -21,17 +21,44 @@ namespace WeakReferences
             newDelegate = new CreateNewDelegate();
         }
 
-        public bool IsDelegateAlive { get { return weakReference.IsAlive; } }
+        public bool IsDelegateAlive 
+        { 
+            get 
+            { 
+                return weakReference.IsAlive; 
+            } 
+        }
 
-        public Delegate Weak { get { return newDelegate.SetNewDelegate(methodInfo, weakReference); } }
+        public Delegate Weak 
+        { 
+            get 
+            { 
+                return newDelegate.SetNewDelegate(methodInfo, weakReference); 
+            } 
+        }
         
         private class CreateNewDelegate
         {
             private MethodInfo delegate_MethodInfo;
             private WeakReference delegate_WeakReference;
            
+            private Type GetDelegateType()
+            {
+                var delegateArguments = new List<Type>(delegate_MethodInfo.GetParameters().Select(param => param.ParameterType));
+                delegateArguments.Add(delegate_MethodInfo.ReturnType);
+
+                return Expression.GetDelegateType(delegateArguments.ToArray());
+            }
+
+            private ParameterExpression[] GetDelegateParameters() 
+            {
+                return delegate_MethodInfo.GetParameters().Select(parameters => Expression.Parameter(parameters.ParameterType)).ToArray();
+            }
+
             private Delegate DelegateInfo()
             {
+                var delegateParameters = GetDelegateParameters();
+                return Expression.Lambda(GetDelegateType(), BlockDelegateAction(delegateParameters), delegateParameters).Compile();
             }
 
             public Delegate SetNewDelegate(MethodInfo methodInfo, WeakReference weakReference)
@@ -40,6 +67,39 @@ namespace WeakReferences
                 this.delegate_WeakReference = weakReference;
                 return DelegateInfo();
             }
-        }       
+
+            private MemberExpression GetDelegateTarget() 
+            {
+                return Expression.Property(Expression.Convert(Expression.Constant(delegate_WeakReference), typeof(WeakReference)), "Target");
+            }
+
+            private MemberExpression GetDelegateIsAlive() 
+            {
+                return Expression.Property(Expression.Convert(Expression.Constant(delegate_WeakReference), typeof(WeakReference)), "IsAlive");
+            }
+
+            private MethodCallExpression CallDelegate(ParameterExpression[] parameters) 
+            {
+                return Expression.Call(instance: Expression.Convert(GetDelegateTarget(), delegate_MethodInfo.DeclaringType), method: delegate_MethodInfo, arguments: parameters);
+            }
+
+            private Expression[] CallDelegateAction(ParameterExpression[] parameters) 
+            { 
+                return new Expression[] 
+                { 
+                    CallDelegate(parameters) 
+                }; 
+            }
+
+            private ConditionalExpression BlockDelegateAction(ParameterExpression[] parameters) 
+            {
+                return Expression.IfThen(Expression.IsTrue(GetDelegateIsAlive()), Expression.Block(GetArgumentList(parameters), CallDelegateAction(parameters))); 
+            }
+
+            private List<ParameterExpression> GetArgumentList(ParameterExpression[] argumentsType) 
+            {
+                return new List<ParameterExpression>(argumentsType.Select(argument => Expression.Variable(argument.Type)));
+            }
+        } 
     }
 }
